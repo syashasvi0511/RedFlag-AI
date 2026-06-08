77618c3c63f7e9d36d8ccda7b8f5db3051c720a8
Yashasvi Sharma <syashasvi0511@gmail.com>
2026-06-08 16:54:36 +0530

    Create Download Dataset.py

---
+++ Download Dataset.py
@@ -0,0 +1,3 @@
+# Download the correct dataset
+!kaggle datasets download -d jocelyndumlao/anerbc-anemia-diagnosis-using-rbc-images
+!unzip -q anerbc-anemia-diagnosis-using-rbc-images.zip -d AneRBC-II
