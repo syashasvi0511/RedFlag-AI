00d2736b39dc058a344ceeec7ae634fa4573546b
Yashasvi Sharma <syashasvi0511@gmail.com>
2026-06-08 16:51:05 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -1,1 +1,15 @@
+import os
 
+os.makedirs("/root/.kaggle", exist_ok=True)
+
+# Paste your token here
+token = "YOUR_KAGGLE_TOKEN"
+username = "syashasvi0511"  # ← replace this
+
+kaggle_json = f'{{"username":"{username}","key":"{token}"}}'
+
+with open("/root/.kaggle/kaggle.json", "w") as f:
+    f.write(kaggle_json)
+
+os.chmod("/root/.kaggle/kaggle.json", 0o600)
+print("Credentials saved!")
